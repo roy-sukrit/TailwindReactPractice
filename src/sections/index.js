6478a3f0cd82af44bfb43f8232373e3550924e1f
“roy-sukrit” <roysukrit78@gmail.com>
2024-09-04 12:59:15 +0100

    Started Tailwind Nike Website

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import PopularProducts from './PopularProducts';
+
+export{
+    Hero,
+    SpecialOffer,
+    CustomerReviews,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    Services,
+    PopularProducts
+}
